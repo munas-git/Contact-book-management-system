85ab841a162865432449ffd9117028a500b96184
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-08-13 22:09:30 +0100

    Setup server side for sing-in page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/home/')
+@app.route('/signin/', methods= ["POST", "GET"])
 def home():
-    return render_template()
+
+    if request.method == "GET":
+        return render_template("signin.html")
+
+    else:
+        user_name = request.form.get("username")
+        password = request.form.get("password")
+        print("User name is: ",user_name)
+        print("Password is: ", password)
+        return f"Thank you, '{user_name}', your login was succesful."
 
 
 if __name__ == '__main__':
